102a1d1f61852f77e66dede2a036d4a9b168fea5
Caroline Suen <cysuen@stanford.edu>
2012-11-07 14:38:35 -0800

    week6

---
+++ week5/footer.php
@@ -0,0 +1,10 @@
+<div data-role="footer" data-id="same bar" class="nav-glyphish-example" data-position="fixed" data-tap-toggle="false">
+	<div data-role="navbar" class="nav-glyphish-example" data-grid="c">
+	<ul>
+		<li><a href="index.php" id="home" data-icon="custom">Home</a></li>
+		<li><a href="login.php" id="key" data-icon="custom">Login</a></li>
+		<li><a href="filter.php" id="beer" data-icon="custom" class="ui-btn-active">Filter</a></li>
+		<li><a href="#" id="skull" data-icon="custom">Settings</a></li>
+	</ul>
+	</div>
+</div>
