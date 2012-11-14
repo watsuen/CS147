abd528aa38eebaa89b7074650ede50cc76176bad
Caroline Suen <cysuen@stanford.edu>
2012-11-14 15:07:40 -0800

    week8

--- week8/sandbox/index.php
+++ week8/sandbox/index.php
@@ -1,3 +1,12 @@
+<?php 
+include 'Mobile_Detect.php';
+$detect = new Mobile_Detect();
+
+if (!$detect->isMobile()) {
+  header("Location: mobiletest/iphone.php?url=stanford.edu/~cysuen/cgi-bin/CS147/week8/sandbox/mobile.php");
+}
+?>
+
 <!DOCTYPE html> 
 <html>
 
